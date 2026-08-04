a9e0f32951e549efccb15a2f3d81767ad9adf743
GuhDul-afk <darkcupio@gmail.com>
2026-08-04 21:06:48 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+// Service worker minimal : met en cache les fichiers de l'appli pour qu'elle
+// s'ouvre même sans connexion, et pour que les navigateurs/PWABuilder la
+// reconnaissent comme une vraie PWA installable.
+const CACHE_NAME = 'carnet-de-bord-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
